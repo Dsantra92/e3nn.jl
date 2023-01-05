d1803e87d8372e5b2c0885006407d16b8f184681
Deeptendu Santra <deeptendu.santra@protonmail.com>
2023-01-05 13:54:23 +0530

    Added more inter-coversions (untested)

--- src/o3/o3.jl
+++ src/o3/o3.jl
@@ -5,8 +5,12 @@ export Irrep, Irreps, dim, isscalar, spherical_harmonics, simplify
 export remove_zero_multiplicities, num_irreps, ls, lmax
 # not implemented yet
 export D_from_angles, D_from_quaternion, D_from_axis_angle, D_from_matrix
+export euler_sangles
 
 include("rotations.jl")
 using .rot
+export Quaternion, QuatRotation
+export RotMatrix, RotMatrixGenerator
+export SphericalFromCartesian, CartesianFromSpherical
 
 end
--- src/o3/rotations.jl
+++ src/o3/rotations.jl
@@ -2,10 +2,60 @@ module rot
 
 using Quaternions
 using Rotations
-using CoordinateTransformations
+using StaticArrays
 
 export Quaternion, QuatRotation
-export RotMatrix, RotMatrixGenerator
-export SphericalFromCartesian, CartesianFromSpherical
+export RotMatrix
+export euler_angles
+
+# Cross conversion
+
+function (::Type{Q})(aa::AngleAxis) where Q <: Quaternion
+    s, c = sincos(aa.theta / 2)
+    return Q(c, s * aa.axis_x, s * aa.axis_y, s * aa.axis_z)
+end
+
+function (::Type{AA})(q::Quaternion) where AA <: AngleAxis
+    AA(QuatRotation(q, false)) # already implemented method
+end
+
+function (::Type{AA})(;α::Real, β::Real, γ::Real) where AA <: AngleAxis
+    AA(RotYZY(promote(α, β, γ)...))
+end
+
+function (::Type{Q})(;α::Real, β::Real, γ::Real) where Q <: Quaternion
+    return QuatRotation(RotYZY(promote(α, β, γ)...)).q |> Q
+end
+
+function euler_angles(x::T) where T <: Union{QuatRotation, RotMatrix3, AngleAxis}
+    Rotations.params(RotYZY(x))
+end
+
+euler_angles(q::Q) where Q<:Quaternion = euler_angles(QuatRotation(q, false))
+
+function (::Type{R})(q::Quaternion) where R <: RotMatrix3
+    QuatRotation(q) |> R
+end
+
+function (::Type{Q})(R::RotMatrix3) where Q <: Quaternion
+    return QuatRotation(R).q |> Q
+end
+
+function CartesianToSphericalAngles(x::AbstractVector{T}) where T<: Real
+    length(x) == 3 || error("Spherical transform takes a 3D coordinate")
+
+    # done in e3nn to remove NaNs
+    # need to check for Julia
+    normalize!(x, p=2)
+    clamp!(x, -1, 1)
+    return SVector(acosx[2], atan(x[1], x[3]))
+end
+
+function SphercialAnglesToCartesian(α::Real, β::Real)
+    α, β = promote(α, β)
+    sβ, cβ = sincos(β)
+    sα, cα = sincos(α)
+    return SVector(sβ * sα, cβ, sβ * cα)
+end
 
 end
